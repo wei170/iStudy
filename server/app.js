ba77dbe3180aa6712c8a80a5b1705430e2723add
wei170 <guocheng@guochengwei.com>
2016-10-02 03:06:51 -0400

    Connect frontend to backend

--- server/app.js
+++ server/app.js
@@ -22,28 +22,30 @@ app.set('port', PORT);
 //     force: true
 // });
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
 // app.engine("html", require("ejs").__express); // or   app.engine("html",require("ejs").renderFile);
 // //app.set("view engine","ejs");
 // app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'views'));
-app.engine("html", require("ejs").__express);
+// app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'ejs');
-app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('views', path.join(__dirname, '../public/'));
+app.engine("html", require("ejs").__express);
+app.set('view engine', 'html');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../')));
 
 
 //set route
 app.use('/', routes);
 app.use('/users', users);
+
+// app.use('/client', express.static(path.join(__dirname, '../client')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
