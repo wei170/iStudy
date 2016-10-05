9ebe4d7e13ec0bd7476d8f18a996505c79255628
maoxia <luo149@purdue.edu>
2016-10-05 06:38:56 -0400

    able to update birthday with right format as YYYY-MM-DD into db

--- models/seedProfile.js
+++ models/seedProfile.js
@@ -3,6 +3,7 @@ module.exports = {
         major: 'CS',
         language: 'English',
         visibility: 'true',
+        birthday: '1996-01-02', // YYYY-MM-DD format
         hobby: '[basketball, movie, computer]',
         classes: '[cs307]',
         posts: '[]',
