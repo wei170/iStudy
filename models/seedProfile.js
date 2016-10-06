7bd3a51fda83fce838029f612deba9e33d529cbf
maoxia <luo149@purdue.edu>
2016-10-06 00:33:54 -0400

    working on fixing the merging err

---
+++ models/seedProfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+    p1: {
+        major: 'CS',
+        language: '[English]',
+        visibility: 'true',
+        birthday: '1996-01-02', // YYYY-MM-DD format
+        hobby: '[basketball, movie, computer]',
+        classes: '[cs307]',
+        posts: '[]',
+        friends: '[]',
+        contributions: '[]'
+    }
+};
