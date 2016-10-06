379fc0f88984e9666cd8ee65cb9a4848c8205b1c
maoxia <luo149@purdue.edu>
2016-10-06 07:44:14 -0400

    Get back to state before the unwanted pull from guocheng branch

--- client/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app',
-	templateUrl: '/client/app.component.html'
-})
-
-export class AppComponent { }
---
+++ routes/users.js
@@ -0,0 +1,40 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+var db = require(__dirname + '/../db.js');
+var _ = require('underscore');
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+    console.log('-=-=------');
+    var body = _.pick(req.body, 'userName','email', 'password');
+
+    db.user.create(body).then(function(user) {
+        res.json(user.toPublicJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
+
+    // console.log('heere');
+    // res.status(200).send();
+});
+
+router.post('/login', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.authenticate(body).then(function(user) {
+        var token = user.generateToken('authentication');
+        console.log('token= '+token);
+        if (token) {
+            res.header('Auth', token).json(user.toPublicJSON());
+        } else {
+            res.status(401).send();
+        }
+
+    }, function() {
+        res.status(401).send();
+
+    });
+});
+
+module.exports = router;
